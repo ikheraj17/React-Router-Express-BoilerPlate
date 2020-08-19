be2ea247122636e326e3650512ec6034fa586484
Imran Kheraj <ikheraj17@gmail.com>
2020-08-18 22:08:19 -0500

    Navbar example component created

--- Client/App.jsx
+++ Client/App.jsx
@@ -5,12 +5,12 @@ import history from './history';
 import axios from 'axios';
 import Home from './Components/pages/Home';
 import One from './Components/pages/One';
+import Nav from './Components/menus/Nav';
 
 const App = () => {
-
-
     return (
         <Router history={history}>
+            <Route path='/' component={Nav} />
             <Route path= '/' exact component={Home} />
             <Route path= '/linkone' exact component={One} />
         </Router>
---
+++ Client/Components/menus/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+    <div>
+        <div>This is the navbar component</div>
+    </div>
+    )
+}
+
+export default Nav;
